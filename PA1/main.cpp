89541d6ba40bd627a577c46129d97127d4c4a043
Zach <zachef221@gmail.com>
2021-09-13 10:30:18 -0600

    git ignore and start of PA1

---
+++ PA1/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+
+using namespace std;
+
+string verifyArgs(int argc, char* argv[]) {
+  if (argc != 6) {
+    return "Incorrect number of arguments provided: " + to_string(argc - 1) + "\n";
+  }
+  string cipherType = argv[1];
+  if (cipherType != "B" && cipherType != "S") {
+    return "Invalid first argument: \"" + cipherType + "\" expected B or S\n";
+  }
+  string inputFile = argv[2];
+  if (!filesystem::exists(inputFile)) {
+    return "Input file \"" + inputFile + "\" does not exist\n";
+  }
+  if (!string(argv[3]).length()) {
+    return "Output filename is not a valid string\n";
+  }
+  string keyFile = argv[4];
+  if (!filesystem::exists(keyFile)) {
+    return "Key file \"" + keyFile + "\" does not exist\n";
+  }
+  string operationMode = argv[5];
+  if (operationMode != "E" && operationMode != "D") {
+    return "Mode of operation is invalid: \"" + operationMode +"\" expected E or D\n";
+  }
+  return "";
+}
+
+string readFile(string filename) {
+  string fileContents;
+  fstream inStream(filename, fstream::in);
+  stringstream tempStream;
+  tempStream << inStream.rdbuf();
+  return tempStream.str();
+}
+
+int main(int argc, char *argv[]) {
+  string argError = verifyArgs(argc, argv);
+  if (argError.length()) {
+    cout << argError << endl;
+    return -1;
+  }
+  string cipherType = argv[1];
+  string inputFile = argv[2];
+  string outputFile = argv[3];
+  string keyFile = argv[4];
+  string operationMode = argv[5];
+  fstream outStream(outputFile, fstream::out);
+  string plaintext = readFile(inputFile);
+  string key = readFile(keyFile);
+  cout << (int) key[key.size()-1];
+  outStream << plaintext;
+  outStream << plaintext;
+}
