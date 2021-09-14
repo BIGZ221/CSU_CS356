1b447c731337a9f36d849a0e43b1b2cba0ba2648
Zach Fuchs <zachef221@gmail.com>
2021-09-14 11:23:33 -0600

    60% done

--- PA1/main.cpp
+++ PA1/main.cpp
@@ -43,21 +43,142 @@ string readFile(string filename, bool includeEndNewline = true) {
   return fileContents;
 }
 
+// returns padded 16 byte string or "" if end of file reached
+string getPaddedBlock(fstream &inFile) {
+  int paddingByte = 0x81;
+  int blockSize = 16;
+  char block[blockSize + 1];
+  block[blockSize] = 0x00;
+  inFile.read(block, blockSize);
+  int readCount = inFile.gcount();
+  if (readCount == 0) {
+    return "";
+  }
+  if (readCount < blockSize) {
+    for (int i = readCount; i < blockSize; i++) {
+      block[i] = paddingByte;
+    }
+  }
+  return block;
+}
+
+string getBlock(string &inputText, int offset) {
+  size_t blockSize = 16;
+  char block[blockSize + 1];
+  block[blockSize] = 0x00;
+  inputText.copy(block, blockSize, offset);
+  return block;
+}
+
+// returns padded 16 byte string
+string getPaddedBlock(string &inputText, int offset) {
+  int paddingByte = 0x81;
+  size_t blockSize = 16;
+  string block = getBlock(inputText, offset);
+  if (inputText.size() < blockSize) {
+    for (size_t i = inputText.size(); i < blockSize; i++) {
+      block[i] = paddingByte;
+    }
+  }
+  return block;
+}
+
+void swap(int indexA, int indexB, string &value) {
+  char temp = value[indexA];
+  value[indexA] = value[indexB];
+  value[indexB] = temp;
+}
+
+string blockEncrypt(string key, string plaintext) {
+  string cipher = "";
+  for (size_t i = 0; i < key.size(); i++) {
+    cipher += key[i] ^ plaintext[i];
+  }
+  int start = 0;
+  int end = cipher.size() - 1;
+  int keyIndex = 0;
+  while (start != end) {
+    bool shouldSwap = key[keyIndex] % 2;
+    if (shouldSwap) {
+      swap(start, end, cipher);
+      start++;
+      end--;
+    } else {
+      start++;
+    }
+    keyIndex = (keyIndex + 1) % key.size();
+  }
+  return cipher;
+}
+
+string blockDecrypt(string key, string cipherText) {
+  string plaintext = "";
+  string cipherCopy = cipherText;
+  int start = 0;
+  int end = cipherCopy.size() - 1;
+  int keyIndex = 0;
+  while (start != end) {
+    bool shouldSwap = key[keyIndex] % 2;
+    if (shouldSwap) {
+      swap(start, end, cipherCopy);
+      start++;
+      end--;
+    } else {
+      start++;
+    }
+    keyIndex = (keyIndex + 1) % key.size();
+  }
+  for (size_t i = 0; i < key.size(); i++) {
+    plaintext += key[i] ^ cipherCopy[i];
+  }
+  int padding = plaintext.find_first_of(0x81);
+  plaintext = plaintext.substr(0, padding);
+  return plaintext;
+}
+
+string blockCipher(string key, string inputText, char operationMode) {
+  switch (operationMode) {
+    case 'E': {
+      string cipherText = "";
+      for (size_t i = 0; i < inputText.size(); i += 16) {
+        cipherText += blockEncrypt(key, getPaddedBlock(inputText, i));
+      }
+      return cipherText;
+    }
+    case 'D': {
+      string plainText = "";
+      for (size_t i = 0; i < inputText.size(); i += 16) {
+        plainText += blockDecrypt(key, getBlock(inputText, i));
+      }
+      return plainText;
+    }
+    default:
+      return "";
+  }
+}
+
 int main(int argc, char *argv[]) {
   string argError = verifyArgs(argc, argv);
   if (argError.length()) {
     cout << argError << endl;
     return -1;
   }
-  string cipherType = argv[1];
+  char cipherType = *argv[1];
   string inputFile = argv[2];
   string outputFile = argv[3];
   string keyFile = argv[4];
-  string operationMode = argv[5];
-  fstream outStream(outputFile, fstream::out);
-  string plaintext = readFile(inputFile);
+  char operationMode = *argv[5];
   string key = readFile(keyFile, false);
-  cout << key << "\n";
-  outStream << plaintext;
-  outStream << plaintext;
+  string inputText = readFile(inputFile);
+  string outputText;
+  switch (cipherType) {
+    case 'B':
+      outputText = blockCipher(key, inputText, operationMode);
+      cout << (int)outputText[outputText.size() - 5] << '\n';
+      break;
+    case 'S':
+      break;
+  }
+  fstream outStream(outputFile, fstream::out);
+  outStream << outputText;
 }
